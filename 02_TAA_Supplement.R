2188d655954826a83f479d743844f686a3137b47
Spencer Rose <ser624@lehigh.edu>
2022-10-02 17:11:43 -0400

    Stage changes

--- 02_TAA_Supplement.R
+++ 02_TAA_Supplement.R
@@ -15,18 +15,18 @@ cat("\014")
 # We'll use package readr, which is part of the tidyverse
 library(tidyverse)
 
-# Storing the path of the current working directory
-Temp <- getwd()
-
-# Switching the working directory to the covid-19-data subfolder
-setwd("./covid-19-data/")
-
-# Reading the us.counties.csv in as a data frame
-COUNTIES <- read_csv("us-counties.csv")
-
-# Switching the working directory back to the project folder
-setwd(Temp)
-
+# # Storing the path of the current working directory
+# Temp <- getwd()
+# 
+# # Switching the working directory to the covid-19-data subfolder
+# setwd("./covid-19-data/")
+# 
+# # Reading the us.counties.csv in as a data frame
+# COUNTIES <- read_csv("us-counties.csv")
+# 
+# # Switching the working directory back to the project folder
+# setwd(Temp)
+# 
 # Alternative way to access subfolders
 library(here)
 COUNTIES <- read_csv(here("covid-19-data","us-counties.csv"))
